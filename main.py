fcce88b55354e3592911659dd873c1705fb59685
mikeyoknow <114947956+mikeyoknow@users.noreply.github.com>
2023-05-11 08:48:59 -0400

    changed to rotation

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
 import tkinter as tk
 
 class FirstWindow:
-    def __init__(self, master):
-        self.master = master
-        master.title("First Window")
+    def __init__(self):
+        super().__init__()
+        self.title("Rotation System")
+        self.geometry("300x200")
 
-        self.button = tk.Button(master, text="Open Second Window", command=self.open_second_window)
-        self.button.pack()
+        self.label = tk.Label(self, text="Welcome to Rotation 1.0!")
+        self.label.pack(pady=10)
 
-    def open_second_window(self):
-        self.master.destroy()  # close the first window
-        second_window = tk.Tk()
-        SecondWindow(second_window)
+        self.label_num = tk.Label(self, text="Number of staff working today: ")
+        self.label_num.pack()
 
-class SecondWindow:
-    def __init__(self, master):
-        self.master = master
-        master.title("Second Window")
+        self.entry_num = tk.Entry(self)
+        self.entry_num.pack()
 
-        self.label = tk.Label(master, text="This is the second window.")
-        self.label.pack()
+        self.submit = tk.Button(self, text="Submit", command=self.dataCollection)
 
-        self.button = tk.Button(master, text="submit", command=self.open_third_window)
-        self.button.pack()
-
-    def open_third_window(self):
-        self.master.destroy()  # close the first window
-        third_window = tk.Tk()
-        ThirdWindow(third_window)
-
-class ThirdWindow:
-    def __init__(self, master):
-        self.master = master
-        master.title("Rotaion - press 'next'")
-
+    def dataCollection(self):
+        try:
+            num_people = int(self.entry_num.get())
+            if num_people > 0:
+                self.withdraw()
+                name_input_window = NameWindow(self, num_people)
+                name_input_window.grab_set()
+        except ValueError:
+            pass
 
 root = tk.Tk()
 FirstWindow(root)
