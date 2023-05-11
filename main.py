b36692edccb8a0cab8a6ecfdaee08a71610ed0fa
Erfan Razmand <erfanraz2211raz@gmail.com>
2023-05-10 21:01:26 -0700

    none

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ class SecondWindow:
         self.label = tk.Label(master, text="This is the second window.")
         self.label.pack()
 
+        self.button = tk.Button(master, text="submit", command=self.open_third_window)
+        self.button.pack()
+
+    def open_third_window(self):
+        self.master.destroy()  # close the first window
+        third_window = tk.Tk()
+        ThirdWindow(third_window)
+
+class ThirdWindow:
+    def __init__(self, master):
+        self.master = master
+        master.title("Rotaion - press 'next'")
+
+
 root = tk.Tk()
 FirstWindow(root)
 root.mainloop()
