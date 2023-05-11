8a0c626c8774579d29f011d6c4576419c07d0d74
mikeyoknow <114947956+mikeyoknow@users.noreply.github.com>
2023-05-11 09:10:02 -0400

    added fonts

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ class FirstWindow(tk.Tk):
         self.title("Rotation System")
         self.geometry("300x200")
 
-        self.label = tk.Label(self, text="Welcome to Rotation 1.0!")
+        self.label = tk.Label(self, text="Welcome to Rotation 1.0!", font=("Arial", 14))
         self.label.pack(pady=10)
 
-        self.label_num = tk.Label(self, text="Number of staff working today: ")
+        self.label_num = tk.Label(self, text="Number of staff working today:", font=("Arial", 12))
         self.label_num.pack()
 
-        self.entry_num = tk.Entry(self)
+        self.entry_num = tk.Entry(self, font=("Arial", 12))
         self.entry_num.pack()
 
-        self.submit = tk.Button(self, text="Submit", command=self.dataCollection)
+        self.submit = tk.Button(self, text="Submit", command=self.dataCollection, font=("Arial", 12))
         self.submit.pack(pady=10)
-        
+
     def dataCollection(self):
         try:
             num_people = int(self.entry_num.get())
@@ -27,31 +27,34 @@ class FirstWindow(tk.Tk):
                 name_input_window.grab_set()
         except ValueError:
             pass
+
+
 class NameWindow(tk.Toplevel):
     def __init__(self, parent, num_people):
         super().__init__(parent)
         self.title("Enter Names")
         self.geometry("300x200")
 
-        self.enteries = []
+        self.entries = []
         self.labels = []
         for i in range(num_people):
-            label = tk.Label(self, text=f"Person {i+1}:")
-            label.grid(row=i, column=0)
+            label = tk.Label(self, text=f"Person {i+1}:", font=("Arial", 12))
+            label.grid(row=i, column=0, pady=5, padx=10)
             self.labels.append(label)
 
-            entry = tk.Entry(self)
-            entry.grid(row=i, column=1)
-            self.enteries.append(entry)
-        
-        self.button_submit = tk.Button(self, text="Submit", command=self.display_names)
+            entry = tk.Entry(self, font=("Arial", 12))
+            entry.grid(row=i, column=1, pady=5)
+            self.entries.append(entry)
+
+        self.button_submit = tk.Button(self, text="Submit", command=self.display_names, font=("Arial", 12))
         self.button_submit.grid(row=num_people, column=1, pady=10)
 
     def display_names(self):
-        names = [entry.get() for entry in self.enteries]
+        names = [entry.get() for entry in self.entries]
         self.destroy()
         display_window = DisplayWindow(self.master, names)
         display_window.grab_set()
+
 
 class DisplayWindow(tk.Toplevel):
     def __init__(self, parent, names):
@@ -62,14 +65,16 @@ class DisplayWindow(tk.Toplevel):
         self.names = names
         self.index = 0
 
-        self.label = tk.Label(self, text=names[self.index])
+        self.label = tk.Label(self, text=names[self.index], font=("Arial", 14))
         self.label.pack(pady=20)
 
-        self.button = tk.Button(self, text="Next", command=self.next_name)
+        self.button = tk.Button(self, text="Next", command=self.next_name, font=("Arial", 12))
         self.button.pack(pady=10)
+
     def next_name(self):
         self.index = (self.index + 1) % len(self.names)
         self.label.config(text=self.names[self.index])
+
 
 if __name__ == "__main__":
     First_Window = FirstWindow()
