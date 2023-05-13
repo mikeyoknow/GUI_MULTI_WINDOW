4dd36a5b7da4cd4c8922ff3b8f5e9b4d46596086
Erfan Razmand <114947956+mikeyoknow@users.noreply.github.com>
2023-05-13 10:50:24 -0400

    Changed the style and the panel placements
    Changes the style of user prompt and the way that the rotation is displayed and looped through. I will implement the button later.

--- main.py
+++ main.py
@@ -1,81 +1,25 @@
 import tkinter as tk
+from tkinter import messagebox
 
-class FirstWindow(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Rotation System")
-        self.geometry("300x200")
+class RotationApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Staff Rotation")
 
-        self.label = tk.Label(self, text="Welcome to Rotation 1.0!", font=("Arial", 14))
-        self.label.pack(pady=10)
+        #Initial Variables
+        self.staff_list = []
+        self.current_index = 0
 
-        self.label_num = tk.Label(self, text="Number of staff working today:", font=("Arial", 12))
-        self.label_num.pack()
+        #Create left and right frame panels
+        self.left_frame = tk.Frame(self.root)
+        self.right_frame = tk.Frame(self.root)
+        self.left_frame.pack(side="left", fill="both", expand=True)
+        self.right_frame.pack(side="right", fill="both", expand=True)
 
-        self.entry_num = tk.Entry(self, font=("Arial", 12))
-        self.entry_num.pack()
+        #Listbox to displat staff name
+        self.staff_display = tk.Listbox(self.left_frame)
+        self.staff_display.pack(pady=20)
 
-        self.submit = tk.Button(self, text="Submit", command=self.dataCollection, font=("Arial", 12))
-        self.submit.pack(pady=10)
-
-    def dataCollection(self):
-        try:
-            num_people = int(self.entry_num.get())
-            if num_people > 0:
-                self.withdraw()
-                name_input_window = NameWindow(self, num_people)
-                name_input_window.grab_set()
-        except ValueError:
-            pass
-
-
-class NameWindow(tk.Toplevel):
-    def __init__(self, parent, num_people):
-        super().__init__(parent)
-        self.title("Enter Names")
-        self.geometry("300x200")
-
-        self.entries = []
-        self.labels = []
-        for i in range(num_people):
-            label = tk.Label(self, text=f"Person {i+1}:", font=("Arial", 12))
-            label.grid(row=i, column=0, pady=5, padx=10)
-            self.labels.append(label)
-
-            entry = tk.Entry(self, font=("Arial", 12))
-            entry.grid(row=i, column=1, pady=5)
-            self.entries.append(entry)
-
-        self.button_submit = tk.Button(self, text="Submit", command=self.display_names, font=("Arial", 12))
-        self.button_submit.grid(row=num_people, column=1, pady=10)
-
-    def display_names(self):
-        names = [entry.get() for entry in self.entries]
-        self.destroy()
-        display_window = DisplayWindow(self.master, names)
-        display_window.grab_set()
-
-
-class DisplayWindow(tk.Toplevel):
-    def __init__(self, parent, names):
-        super().__init__(parent)
-        self.title("Names Display")
-        self.geometry("300x200")
-
-        self.names = names
-        self.index = 0
-
-        self.label = tk.Label(self, text=names[self.index], font=("Arial", 14))
-        self.label.pack(pady=20)
-
-        self.button = tk.Button(self, text="Next", command=self.next_name, font=("Arial", 12))
-        self.button.pack(pady=10)
-
-    def next_name(self):
-        self.index = (self.index + 1) % len(self.names)
-        self.label.config(text=self.names[self.index])
-
-
-if __name__ == "__main__":
-    First_Window = FirstWindow()
-    First_Window.mainloop()
+        #Entry field and button to add staff names
+        self.name_entry = tk.Entry(self.left_frame)
+        self.name_entry.pack()
