3afd87c311d5fe3cebe218c5252bcebf72441e14
mikeyoknow <114947956+mikeyoknow@users.noreply.github.com>
2023-05-13 14:44:13 -0400

    GUI basics implemented but not the button up and down

--- main.py
+++ main.py
@@ -23,3 +23,43 @@ class RotationApp:
         #Entry field and button to add staff names
         self.name_entry = tk.Entry(self.left_frame)
         self.name_entry.pack()
+        self.add_button = tk.Button(self.left_frame, text="Add Staff", command=self.add_staff)
+        self.add_button.pack()
+        
+        #Buttons to delete names
+        self.del_button = tk.Button(self.left_frame, text="Delete Selected", command=self.del_staff)
+        self.del_button.pack()
+
+        #Buttons to move staff up and down
+        self.up_button = tk.Button(self.left_frame, text="Move up", command=self.move_up)
+        self.up_button.pack()
+        self.down_button = tk.Button(self.left_frame, text="Move down", command=self.move_down)
+        self.down_button.pack()
+
+        #Label to display rotation
+        self.rotation_display = tk.Button(self.right_frame, text="")
+        self.rotation_display.pack(pady=20)
+
+        #Button to rotate staff names
+        self.next_button = tk.Button(self.right_frame, text="Next", command=self.next_staff)
+        self.next_button.pack(pady=100)
+
+    def add_staff(self):
+        new_name = self.name_entry.get()
+        if new_name:
+            self.staff_list.append(new_name)
+            self.staff_display.insert("end", new_name)
+            self.name_entry.delete(0, "end")
+            self.update_rotation_display()
+
+    def del_staff(self):
+        selected_name = self.staff_display.curselection()
+        if selected_name:
+            self.staff_list.pop(selected_name[0])
+            self.staff_display.delete(selected_name)
+            self.update_rotation_display()
+
+    def next_staff(self):
+        if self.staff_list:
+            self.current_index = (self.current_index + 1) % len(self.staff_list)
+            self.update_rotation_display()
