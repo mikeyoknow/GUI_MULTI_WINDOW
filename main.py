9ac820bd824d022e6b8e2949a9fd63e3fbc9deeb
mikeyoknow <114947956+mikeyoknow@users.noreply.github.com>
2023-05-14 14:15:29 -0400

    graphical adjustments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import tkinter.font as tkfont
 from tkinter import messagebox
 
 class RotationApp:
@@ -8,7 +9,7 @@ class RotationApp:
         self.root.title("Staff Rotation")
 
         # Set the window size
-        self.root.geometry("600x480")
+        self.root.geometry("700x400")
 
         # Change the background color
         self.root.configure(bg="#f0f0f0")
@@ -24,38 +25,37 @@ class RotationApp:
         self.right_frame.pack(side="right", fill="both", expand=True, padx=20, pady=20)
 
         # Listbox to display staff names
-        self.staff_display = tk.Listbox(self.left_frame, font=("Arial", 12))
-        self.staff_display.pack(pady=20)
+        self.staff_display = tk.Listbox(self.left_frame, font=("Arial", 12), width=40, height=10)
+        self.staff_display.grid(row=0, column=0, columnspan=4, pady=20)
 
         # Entry field and button to add staff names
-        self.name_entry = ttk.Entry(self.left_frame, font=("Arial", 12))
-        self.name_entry.pack(pady=10)
+        self.name_entry = ttk.Entry(self.left_frame, font=("Arial", 12), width=40)
+        self.name_entry.grid(row=1, column=0, columnspan=4, pady=10)
         self.add_button = ttk.Button(self.left_frame, text="Add Staff", command=self.add_staff)
-        self.add_button.pack(pady=10)
+        self.add_button.grid(row=2, column=0, pady=10)
 
-        # Buttons to indicate staff going for lunch and returning
         self.lunch_button = ttk.Button(self.left_frame, text="Go to Lunch", command=self.go_to_lunch)
-        self.lunch_button.pack(pady=10)
+        self.lunch_button.grid(row=2, column=1, pady=10)
+
         self.back_button = ttk.Button(self.left_frame, text="Back from Lunch", command=self.back_from_lunch)
-        self.back_button.pack(pady=10)
+        self.back_button.grid(row=2, column=2, pady=10)
 
-        # Button to delete selected staff name
         self.del_button = ttk.Button(self.left_frame, text="Delete Selected", command=self.del_staff)
-        self.del_button.pack(pady=10)
+        self.del_button.grid(row=3, column=2, pady=10)
 
-        # Buttons to move staff up and down
         self.up_button = ttk.Button(self.left_frame, text="Move Up", command=self.move_up)
-        self.up_button.pack(pady=10)
+        self.up_button.grid(row=3, column=0, pady=10)
+
         self.down_button = ttk.Button(self.left_frame, text="Move Down", command=self.move_down)
-        self.down_button.pack(pady=10)
+        self.down_button.grid(row=3, column=1, pady=10)
 
         # Label to display rotation
-        self.rotation_display = ttk.Label(self.right_frame, text="", font=("Arial", 24, "bold"))
+        self.rotation_display = tk.Label(self.right_frame, text="??????????????", font=("Arial", 24, "bold"))
         self.rotation_display.pack(pady=20)
 
         # Button to rotate staff names
         self.next_button = ttk.Button(self.right_frame, text="Next", command=self.next_staff)
-        self.next_button.pack(pady=200)
+        self.next_button.pack(pady=100)
 
     def add_staff(self):
         new_name = self.name_entry.get()
